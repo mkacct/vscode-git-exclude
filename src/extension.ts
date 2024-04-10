aff5d347d1fd2969c82fee834ea51c9732602014
Madeline Kahn <mk-acct@outlook.com>
2024-04-10 00:51:27 -0400

    Support multiple repos

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,8 @@
 import * as vscode from "vscode";
+import * as vscodeGit from "./git";
 import {ExtensionCommandError, wrapExtensionCommand} from "./util";
 
+const GIT_EXTENSION_ID = "vscode.git";
 const EXCLUDE_PATH = [".git", "info", "exclude"];
 
 export function activate(context: vscode.ExtensionContext): void {
@@ -9,31 +11,61 @@ export function activate(context: vscode.ExtensionContext): void {
 	context.subscriptions.push(openCmd, excludeCmd);
 }
 
-async function getGitAPI(): Promise<any> {
-	const git = vscode.extensions.getExtension("vscode.git");
-	if (!git) {throw new ExtensionCommandError("Git extension not found");}
-	return (await git.activate()).getAPI(1);
-}
-
-async function getExcludeFullPath(gitApi: any): Promise<vscode.Uri> {
+const openCmdFunc = wrapExtensionCommand(async (): Promise<void> => {
+	const gitApi = await getGitAPI();
 	const repos = gitApi.repositories;
 	if (repos.length == 0) {throw new ExtensionCommandError("No git repository open");}
 	let repo;
 	if (repos.length > 1) {
-		throw new ExtensionCommandError("NYI: multiple git repositories open"); // TODO: implement
+		repo = await pickRepo(repos);
+		if (!repo) {return;}
 	} else {
 		repo = repos[0];
 	}
 	const repoUri = repo.rootUri;
-	return vscode.Uri.joinPath(repoUri, ...EXCLUDE_PATH);
-}
-
-const openCmdFunc = wrapExtensionCommand(async (): Promise<void> => {
-	const gitApi = await getGitAPI();
-	await vscode.window.showTextDocument(await getExcludeFullPath(gitApi));
+	const excludeFullPath = vscode.Uri.joinPath(repoUri, ...EXCLUDE_PATH);
+	await vscode.window.showTextDocument(excludeFullPath);
 });
 
 const excludeCmdFunc = wrapExtensionCommand(async (): Promise<void> => {
 	// TODO: implement
 	throw new ExtensionCommandError("NYI");
 });
+
+async function getGitAPI(): Promise<vscodeGit.API> {
+	const git = vscode.extensions.getExtension<vscodeGit.GitExtension>(GIT_EXTENSION_ID);
+	if (!git) {throw new ExtensionCommandError("Git extension not found");}
+	return (await git.activate()).getAPI(1);
+}
+
+function pickRepo(repos: vscodeGit.Repository[]): Promise<vscodeGit.Repository | null> {
+	return new Promise<vscodeGit.Repository | null>((resolve) => {
+		const quickPick = vscode.window.createQuickPick<RepoQuickPickItem>();
+		quickPick.placeholder = "Choose a repository";
+		quickPick.items = repos.map((repo): RepoQuickPickItem => {
+			const pathTerms = repo.rootUri.path.split("/");
+			return {
+				label: pathTerms[pathTerms.length - 1],
+				description: repo.state.HEAD?.name,
+				repo: repo
+			};
+		});
+		let didAccept = false;
+		quickPick.onDidAccept(() => {
+			if (didAccept) {return;}
+			didAccept = true;
+			const chosenRepo = quickPick.selectedItems[0].repo;
+			quickPick.hide();
+			resolve(chosenRepo);
+		});
+		quickPick.onDidHide(() => {
+			quickPick.dispose();
+			if (!didAccept) {resolve(null);}
+		});
+		quickPick.show();
+	});
+}
+
+interface RepoQuickPickItem extends vscode.QuickPickItem {
+	repo: vscodeGit.Repository;
+}
